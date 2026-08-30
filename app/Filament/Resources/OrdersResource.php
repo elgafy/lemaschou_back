136d0fd45e265a2a2c673bf1aa36a0a71be9e70c
elgafy <abdalaziz.elgafy@gmail.com>
2026-08-30 06:51:35 +0300

    Revert "Added user privileges to reservation resource"
    This reverts commit 62dadb4ed1545ce4c2a042a1020f137365fb2cad.

--- app/Filament/Resources/OrdersResource.php
+++ app/Filament/Resources/OrdersResource.php
@@ -2,7 +2,6 @@
 
 namespace App\Filament\Resources;
 
-use Filament\Facades\Filament;
 use App\Filament\Resources\OrdersResource\Pages;
 use App\Models\Order;
 use Filament\Forms\Components\Select;
@@ -27,30 +26,30 @@ class OrdersResource extends Resource
     public static function canViewAny(): bool
     {
         $user = Filament::auth()->user();
-        return $user ? $user->can('view_any_order') : false;
+        return $user ? $user->can('view_any_reservation') : false;
     }
     public static function canView($record): bool
     {
         $user = Filament::auth()->user();
-        return $user ? $user->can('view_order') : false;
+        return $user ? $user->can('view_reservation') : false;
     }
 
     public static function canCreate(): bool
     {
         $user = Filament::auth()->user();
-        return $user ? $user->can('create_order') : false;
+        return $user ? $user->can('create_reservation') : false;
     }
 
     public static function canEdit($record): bool
     {
         $user = Filament::auth()->user();
-        return $user ? $user->can('update_order') : false;
+        return $user ? $user->can('update_reservation') : false;
     }
 
     public static function canDelete($record): bool
     {
         $user = Filament::auth()->user();
-        return $user ? $user->can('delete_order') : false;
+        return $user ? $user->can('delete_reservation') : false;
     }
 
     public static function form(Form $form): Form
